b04bc1526220bccdbb8bca28432fa93df85b73ec
Wei-Ting Yap <29475856+weitingyap@users.noreply.github.com>
2025-03-15 10:43:09 -0700

    feat: initial commit, instantiate express server

---
+++ app.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => res.send("hello world!"));
+
+const PORT = process.env.PORT | 8000;
+app.listen(PORT, () => {
+	console.log(`listening on ${PORT}`);
+});
