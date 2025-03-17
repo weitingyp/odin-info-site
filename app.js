ae9e0faafea5309ba31124d7c7af2434fac8cddf
Wei-Ting Yap <29475856+weitingyap@users.noreply.github.com>
2025-03-16 19:45:20 -0700

    feat: add static files and about page ejs

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 import express from "express";
+import path from "path";
 
 const app = express();
+
+/* 
+VIEWS - templatized HTML
+- see later lines for rendered templates, arguments passed into the res.locals object when rendering
+- you can write JS logic within <% array.forEach( element => { console.log(element) })
+- you can add template arguments with <%= variablename %> and pass these arguments via the res.locals object
+- you can nest HTML templates in HTML templates using <%- include 'ejsfilenamewithoutextension' %>
+- res.locals object is also the second argument to res.render e.g. res.render('ejsfilename', { arg: arg_val})
+*/
+app.set("views", path.join(import.meta.dirname, "views"));
+app.set("view engine", "ejs");
+
+/* 
+VIEWS - serving static assets with EJS
+- express.static() is a middleware function that enables static assets
+*/
+app.use(express.static(path.join("import.meta.dirname", "static")));
 
 /* 
 ROUTES
@@ -23,7 +41,22 @@ ROUTES
 import authorRouter from "./routes/authorRouter.js";
 app.use("/authors", authorRouter);
 
-app.get("/", (req, res) => res.send("hello world!"));
+// app.js
+
+const links = [
+	{ href: "/", text: "Home" },
+	{ href: "about", text: "About" },
+];
+
+const users = ["Rose", "Cake", "Biff"];
+
+app.get("/", (req, res) => {
+	res.render("index", { links: links, users: users });
+});
+
+app.get("/about", (req, res) => {
+	res.render("about", { links });
+});
 
 const PORT = process.env.PORT | 8000;
 app.listen(PORT, () => {
